1f9bc18e66000f48ac19896c068056f7ff31711c
akmal-sudo <akmalbadgandi@gmail.com>
2025-12-13 12:56:24 +0530

    Create hash_table.cpp

---
+++ hashing/hash_table.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    unordered_map<string, string> m;
+
+    m["AMB101"] = "Free";
+    m["FIRE03"] = "Busy";
+    m["POL12"] = "On Duty";
+
+    cout << m["AMB101"];
+}
