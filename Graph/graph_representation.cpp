2c500e0ac0d0b18745d18f7278c3c34282e78586
akmal-sudo <akmalbadgandi@gmail.com>
2025-12-13 12:49:24 +0530

    Update graph_representation.cpp

--- Graph/graph_representation.cpp
+++ Graph/graph_representation.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n = 5;
+    vector<vector<int>> adj(n);
+
+    adj[0].push_back(1);
+    adj[1].push_back(2);
+    adj[2].push_back(3);
+    adj[3].push_back(4);
+
+    for(int i = 0; i < n; i++) {
+        cout << i << ": ";
+        for(int v : adj[i]) cout << v << " ";
+        cout << endl;
+    }
+}
 
